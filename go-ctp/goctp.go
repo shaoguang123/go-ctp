d39d194845cabfeb36c2427dc82d1409c894b0d5
shaoguang123 <1213698711@qq.com>
2017-01-19 18:21:47 +0800

    add new files

---
+++ go-ctp/goctp.go
@@ -0,0 +1,6 @@
+package goctp
+
+//#cgo LDFLAGS: -fPIC -L. -L/home/shaoguang/go/src/github.com/shaoguang123/go-ctp/lib -Wl,-rpath=/home/shaoguang/go/src/github.com/shaoguang123/go-ctp/lib -lthostmduserapi -lthosttraderapi -lstdc++
+//#cgo CPPFLAGS: -fPIC -I. -I/home/shaoguang/go/src/github.com/shaoguang123/go-ctp/api/ThostTraderApi_v6.3.6_20160606_linux64
+import "C"
+
